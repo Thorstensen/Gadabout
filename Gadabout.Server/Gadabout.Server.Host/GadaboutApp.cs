d90efd32ad413cf97512c80b62e0b8058b4c8918
Tobias Moe Thorstensen <tobias.thorstensen@gmail.com>
2017-07-01 20:34:06 +0200

    First server commit..

---
+++ Gadabout.Server/Gadabout.Server.Host/GadaboutApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gadabout.Server.Host
+{
+    public class GadaboutApp
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting Gadabout Server Application");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping Gadabout Server Application");
+        }
+    }
+}
---
+++ Gadabout.Server/Gadabout.Server.Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Gadabout.Server.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<GadaboutApp>(gadabout =>
+                {
+                    gadabout.ConstructUsing(app => new GadaboutApp());
+                    gadabout.WhenStarted(cfg => cfg.Start());
+                    gadabout.WhenStopped(cfg => cfg.Stop());
+                });
+
+                configure.SetDescription("Gadabout Server responsible for hosting Web Services and database transactions");
+                configure.SetDisplayName("Gadabout Server");
+                configure.SetServiceName("Gadabout Server");
+            });
+        }
+    }
+}
