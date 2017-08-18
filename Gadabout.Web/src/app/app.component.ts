30106ff25e5bbebc024b27552e89b5e0edcd1a22
Tobias Moe Thorstensen <tobias.thorstensen@gmail.com>
2017-08-18 15:14:07 +0200

    including my first angular component in the app.component. Yay.

--- Gadabout.Web/src/app/app.component.ts
+++ Gadabout.Web/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {TripListComponent} from './trips/trip-list.component'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }      from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`<h1><trip-list></trip-list></h1>`,
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'app';
